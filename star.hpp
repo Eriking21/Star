dc04c4848c808080fd67130e64968593a5383fdd
Eriking21 <64511659+Eriking21@users.noreply.github.com>
2025-02-18 05:52:11 +0100

    Create star.hpp

---
+++ star.hpp
@@ -0,0 +1,27 @@
+/**
+ * Copyright 2025 Erivaldo Jair Xavier Mate
+ * @brief If you cant deal with performance search for "unsafe"
+ **/
+
+#pragma once
+
+#ifndef mk_table
+#define mk_table(NAME) struct NAME##_Format* NAME;
+#endif
+
+#ifdef EXPORT_ERIM_NAMESPACE
+namespace erim::star{
+#endif
+
+template<auto& vector,typename Idx,unsigned qtd=1>
+struct Link{  
+  Idx keys[qtd];
+  decltype(*vector)& operator[](auto i=0){
+    return vector[keys[i]]; //unsafe
+  }
+}
+
+#ifdef USE_ERIM_NAMESPACE
+}
+#endif
+
